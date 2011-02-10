2e4ab165b2b556f19d6fe6b32f1906c86be105ef
rivan <roman.e.ivanov@gmail.com>
2011-02-10 21:24:07 +0300

    enable openid features

--- src/w3fu/__init__.py
+++ src/w3fu/__init__.py
@@ -8,14 +8,21 @@ from w3fu.data.xml import XSLT
 from w3fu.storage import Storage
 from w3fu.storage.dbapi import Connection
 from w3fu.storage.orm.auth import Users, Sessions
+from w3fu.storage.orm._openid import OpenIdStore
 
 from w3fu.res import Controller
 from w3fu.res.index import Index
 from w3fu.res.test import HtmlTest, JsonTest
 from w3fu.res.auth import Login, Register
+from w3fu.res._openid import OpenIdAuth
 
 
-controller = Controller([Index, JsonTest, HtmlTest, Login, Register])
+controller = Controller([Index,
+                         JsonTest,
+                         HtmlTest,
+                         Login,
+                         Register,
+                         OpenIdAuth])
 
 conn_config = {
                'host': config.conn_host,
@@ -31,8 +38,9 @@ storage = Storage(lambda storage: Connection(
                                              storage=storage,
                                              driver=MySQLdb,
                                              config=conn_config,
-                                             mappers=(Users, Sessions)
-                                             ))
+                                             mappers=(Users,
+                                                      Sessions,
+                                                      OpenIdStore)))
 
 xslt = XSLT(config.xsl_path)
 
