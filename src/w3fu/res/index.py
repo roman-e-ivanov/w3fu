1f57be7115515a89101389d54caf592a83bce3ce
rivan <roman.e.ivanov@gmail.com>
2011-02-15 14:13:32 +0300

    pass user to index

--- src/w3fu/res/index.py
+++ src/w3fu/res/index.py
@@ -1,11 +1,11 @@
 from w3fu.res import bind, Resource
-from w3fu.res.snippets import html
+from w3fu.res.snippets import html, user
 
 
 @bind('/')
 class Index(Resource):
 
     @html('index-html')
-
+    @user
     def get(self):
         return self.req.response(200, {})
