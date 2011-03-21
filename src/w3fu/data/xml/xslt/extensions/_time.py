55369395cc2dbb18392d6eef8807adacde5265b1
rivan <roman.e.ivanov@gmail.com>
2011-03-21 17:18:18 +0300

    index required

--- src/w3fu/data/xml/xslt/extensions/_time.py
+++ src/w3fu/data/xml/xslt/extensions/_time.py
@@ -4,5 +4,5 @@ from time import time as _time, gmtime as _gmtime
 def time(_):
     return _time()
 
-def gmtime(_, secs=None, index=0):
+def gmtime(_, index, secs=None):
     return _gmtime(secs)[index]
