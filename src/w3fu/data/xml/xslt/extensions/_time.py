25dda9108156af25256d35da73a2a975f80b78f8
rivan <roman.e.ivanov@gmail.com>
2011-03-25 12:06:11 +0300

    cast args to ints

--- src/w3fu/data/xml/xslt/extensions/_time.py
+++ src/w3fu/data/xml/xslt/extensions/_time.py
@@ -4,5 +4,5 @@ from time import time as _time, gmtime as _gmtime
 def time(_):
     return _time()
 
-def gmtime(_, index, secs=None):
-    return _gmtime(secs)[index]
+def gmtime(_, index, secs=_time()):
+    return _gmtime(int(secs))[int(index)]
