2647283a9431380855e163bfb109e3d71d58a0ad
rivan <roman.e.ivanov@gmail.com>
2011-05-27 15:45:07 +0400

    required sessions

--- src/w3fu/res/middleware/context.py
+++ src/w3fu/res/middleware/context.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from w3fu import config
+from w3fu.web import Response
 from w3fu.res.middleware import Middleware
 from w3fu.domain.auth import Session
 
@@ -17,14 +18,17 @@ class storage(Middleware):
 
 class session(Middleware):
 
+    def __init__(self, required=False):
+        self._required = required
+
     def _handler(self, res, req, handler):
         res.session = None
         uid = req.cookie.get(config.session_name)
         if uid is not None:
-            session = Session.find_valid_uid(res.db, uid=uid.value,
+            res.session = Session.find_valid_uid(res.db, uid=uid.value,
                                              time=datetime.utcnow())
-            if session is not None:
-                res.session = session
+        if self._required and res.session is None:
+            return Response(403)
         resp = handler(res, req)
         if res.session is not None and resp.status == 200:
             resp.content['session'] = res.session
