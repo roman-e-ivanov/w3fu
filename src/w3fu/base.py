a2079b9570931159a4e09241980a8a70ce9a1cc8
rivan <nbahob@nbahob-ub>
2012-02-22 17:57:50 +0400

    remove cookie handling from response

--- src/w3fu/base.py
+++ src/w3fu/base.py
@@ -124,12 +124,3 @@ class Response(object):
     def header(self, name, value):
         self.headers.append((name, value))
         return self
-
-    def set_cookie(self, name, value, expires=None, path='/'):
-        morsel = Morsel()
-        morsel.set(name, value, str(value))
-        morsel['path'] = path
-        if expires is not None:
-            morsel['expires'] = expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
-        self.header('Set-Cookie', morsel.OutputString())
-        return self
