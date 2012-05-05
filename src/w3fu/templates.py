3a1c79e81fbb501d58ebff85d362b5009c822a25
rivan <rivan@work>
2012-05-05 19:06:20 +0400

    some templates experiments

--- src/w3fu/templates.py
+++ src/w3fu/templates.py
@@ -21,48 +21,51 @@ class Template(object):
         self._subs = include
         self._subs.update(inline)
 
-    def render(self, ro, rw=None):
-        return self(self._body, ro, rw or {})
+    def render(self, **ctx):
+        return self(self._body, ctx)
 
-    def __call__(self, op, ro, rw):
+    def __call__(self, op, ctx):
         if isinstance(op, dict):
             try:
-                return getattr(self, op[OP_KEY])(op, ro, rw)
+                return getattr(self, op[OP_KEY])(op, ctx)
             except KeyError:
-                return dict([(k, self(v, ro, rw)) for k, v in op.iteritems()])
+                return dict([(k, self(v, ctx)) for k, v in op.iteritems()])
         if isinstance(op, list):
-            return [self(v, ro, rw) for v in op]
+            return [self(v, ctx) for v in op]
         return op
 
-    def call(self, op, ro, rw):
-        params = self(op.get('params', {}), ro, rw)
-        return self._subs[op['name']].render(ro, dict(params))
+    def call(self, op, ctx):
+        params = self(op.get('params', {}), ctx)
+        return self._subs[op['name']].render(**dict(ctx, **params))
 
-    def join(self, op, ro, rw):
-        seq = self(op['list'], ro, rw)
-        return ''.join([str(v) for v in seq])
+    def each(self, op, ctx):
+        lst = self(op['list'], ctx)
+        inner = self(op.get('inner', '.'), ctx)
+        new_ctx = dict(ctx)
+        for v in lst:
+            new_ctx[inner] = v
+            yield(self(op['block'], new_ctx))
 
-    def each(self, op, ro, rw):
-        seq = self(op['list'], ro, rw)
-        return [self(op['block'], v, rw) for v in seq]
+    def join(self, op, ctx):
+        lst = self(op['list'], ctx)
+        separator = self(op.get('separator', ''), ctx)
+        return separator.join([str(v) for v in lst])
 
-    def ro(self, op, ro, rw):
-        path = self(op.get('path', []), ro, rw)
-        res = ro
+    def data(self, op, ctx):
+        path = self(op['path'], ctx)
+        res = ctx
         for p in path:
             res = res[p]
         return res
 
-    def rw(self, op, ro, rw):
-        path = self(op.get('name', []), ro, rw)
-        res = ro
-        for p in path:
-            res = res[p]
-        return res
+    def ifelse(self, op, ctx):
+        test = self(op['test'], ctx)
+        return self(op.get('true' if test else 'false', ''), ctx)
 
-    def ifelse(self, op, ro, rw):
-        cond = self(op['test'], ro, rw)
-        return self(op.get('true' if cond else 'false', ''), ro, rw)
+    def format(self, op, ctx):
+        text = self(op['text'], ctx)
+        args = self(op['args'], ctx)
+        return text.format(**args)
 
 
 template = {
@@ -70,10 +73,10 @@ template = {
                      '': 'join',
                      'list': {
                               '': 'each',
-                              'list': {'': 'ro', 'path': 'x'},
+                              'list': {'': 'data', 'path': ['root', 'x']},
                               'block': {
                                         '': 'join',
-                                        'list': ['static', {'': 'ro', 'path': 'y'}]
+                                        'list': ['static', {'': 'data', 'path': ['.', 'y']}]
                                         }
                               }
                      }
@@ -84,4 +87,61 @@ data = {
         }
 
 t = Template(template)
-print t.render(data)
+print t.render(root=data)
+
+from xml.etree.ElementTree import XMLParser, ElementTree
+
+xml = '''
+<root>
+test1
+<e1/>
+test2
+<e2/>
+test3
+<e3/>
+test4
+</root>
+'''
+
+class Builder(object):
+
+    def close(self):
+        print('close')
+
+    def data(self, data):
+        print('data = ' + str(data))
+
+    def start(self, tag, attrs):
+        print('start tag = ' + str(tag))
+
+    def end(self, tag):
+        print('end tag = ' + str(tag))
+
+
+
+parser = XMLParser(target=Builder())
+parser.feed(xml)
+parser.close()
+
+print('----------')
+
+tree = ElementTree()
+tree.parse("test.xml")
+root = tree.getroot()
+for e in root:
+    print e.tag
+
+
+template = '''
+<template>
+    <template name="t1"></template>
+    <template name="t2" src="...">
+</template>
+'''
+
+
+class Operation(object):
+
+    def __init__(self, static, dynamic):
+        self._static = static
+        self._dynamic = dynamic
