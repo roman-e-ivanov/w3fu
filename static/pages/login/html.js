560f012c4100cc914317a689a199de2f9a957525
striped <striped@work>
2012-05-18 14:43:42 +0400

    devmenu block

--- static/pages/login/html.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('<script type="text/javascript" src="/s/js/jquery.display.js" />').appendTo($('head'));
-	
-	$('.b-form-login').display();
-	$('.b-edit-password__toggle-display').css('display','inline');
-});
