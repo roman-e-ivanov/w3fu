329a0111f94716df22b3bbb27bb72956a74b01e2
striped <striped@work>
2012-05-30 17:32:43 +0400

    new edit & button blocks

---
+++ src/app/config/custom.py
@@ -0,0 +1,18 @@
+from datetime import timedelta
+
+
+xslt_ext_prefix = 'http://w3fu/'
+xsl_path = 'd:/workspace/w3fu/xsl'
+blocks_root = 'd:/workspace/w3fu/blocks'
+
+cli_http_host = 'localhost'
+cli_request_method = 'GET'
+cli_path_info = '/register'
+cli_query_string = ''
+cli_http_cookie = ''
+
+db_uri = 'mongodb://localhost'
+db_name = 'w3fu'
+
+session_cookie = 'u'
+session_ttl = timedelta(days=1)
